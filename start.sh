2c59c057212905d24c36d6bf6ca2ab76196cb480
Girish Ramakrishnan <girish@cloudron.io>
2018-06-20 21:05:33 -0700

    Move customizable settings to config.json
    In addition:
    * run db migration after the config files is generated and setup
    * change ownership after the config is generated

--- start.sh
+++ start.sh
@@ -4,26 +4,22 @@ set -eu
 
 # prepare data directory
 mkdir -p /app/data/uploads /tmp/hackmd /run/hackmd
-chown -R cloudron:cloudron /app/data /tmp/hackmd /run/hackmd
 
 if [ ! -e /app/data/config.json ]; then
+    echo "Creating initial template on first run"
 	cp /app/code/config.json-cloudron /app/data/config.json
-fi
-
-if [ -f .sequelizerc ]; then
-	node_modules/.bin/sequelize db:migrate
 fi
 
 # generate and store an unique sessionSecret for this installation
 CONFIG_JSON=/app/data/config.json
-if [ $(jq .sessionSecret $CONFIG_JSON) == "null" ]; then
-        echo "generating sessionSecret"
-        sessionsecret=$(pwgen -1sc 32)
-        jq ".sessionSecret = \"$sessionsecret\"" $CONFIG_JSON | sponge $CONFIG_JSON
+if [ $(jq .production.sessionSecret $CONFIG_JSON) == "null" ]; then
+    echo "generating sessionSecret"
+    sessionsecret=$(pwgen -1sc 32)
+    jq ".production.sessionSecret = \"$sessionsecret\"" $CONFIG_JSON | sponge $CONFIG_JSON
 fi
 
-export NODE_ENV='production'
-export HMD_ALLOW_ANONYMOUS="false"
+# these cannot be changed by user (https://github.com/hackmdio/hackmd/wiki/Environment-Variables)
+export HMD_DOMAIN="$APP_DOMAIN"
 export HMD_DB_URL="$POSTGRESQL_URL"
 export HMD_LDAP_URL="$LDAP_URL"
 export HMD_LDAP_BINDDN="$LDAP_BIND_DN"
@@ -31,18 +27,17 @@ export HMD_LDAP_BINDCREDENTIALS="$LDAP_BIND_PASSWORD"
 export HMD_LDAP_SEARCHBASE="$LDAP_USERS_BASE_DN"
 export HMD_LDAP_SEARCHFILTER="(username={{username}})"
 export HMD_LDAP_USERNAMEFIELD="username"
+export HMD_PORT=3000
 export HMD_IMAGE_UPLOAD_TYPE=filesystem
-# the following two changes could be transferred to config.json to enable users to change this
-export HMD_EMAIL=false
-export HMD_ALLOW_EMAIL_REGISTER=false
-# disable pdf export until https://github.com/hackmdio/hackmd/issues/820 is fixed
-export HMD_ALLOW_PDF_EXPORT=false
-# respect users privacy and make notes private by default
-export HMD_DEFAULT_PERMISSION=private
-# let users choose to allow guest editing
-export HMD_ALLOW_ANONYMOUS_EDITS=true
-# create new page, if url does not yet exist
-export HMD_ALLOW_FREEURL=true
+export HMD_TMP_PATH=/tmp/hackmd
+
+if [ -f .sequelizerc ]; then
+	node_modules/.bin/sequelize db:migrate
+fi
+
+chown -R cloudron:cloudron /app/data /tmp/hackmd /run/hackmd
 
 # run
+export NODE_ENV=production
 exec /usr/local/bin/gosu cloudron:cloudron node app.js
+
